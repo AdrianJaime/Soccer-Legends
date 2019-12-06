25c7d289db0f70fbed16cad461d9742fccbfffcb
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2019-12-06 19:33:49 +0100

    Card Programming Info

---
+++ Soccer Legends/Assets/Character-Cards/_Scripts/CharacterInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct Stats
+{
+    public int shot;
+    public int technique;
+    public int defense;
+}
+
+public enum Rarity {BRONZE,SILVER,GOLD }
+public enum Type {RED,GREEN,YELLOW,PURPLE,BLUE }
+public enum Rol { defense, TECHNICAL, DEFENSIVE }
+
+
+[CreateAssetMenu(fileName ="New_CharacterInfo", menuName ="Cards/NewInfo")]
+public class CharacterInfo : ScriptableObject
+{
+    //info player
+    public string ID;
+    public string nameCharacter;
+    public string description;
+    public int level;
+    public Rarity rarity;
+    public Type type;
+
+    //info drawable
+    public Sprite artwork;
+
+    //info stats
+    public Stats stats;
+
+    //info special attack
+    public SpecialAttackInfo specialAttackInfo;
+
+}
---
+++ Soccer Legends/Assets/Character-Cards/_Scripts/SpecialAttackInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New_SpecialAttackInfo", menuName = "Cards/NewSpecialAttack")]
+public class SpecialAttackInfo : ScriptableObject
+{
+
+}

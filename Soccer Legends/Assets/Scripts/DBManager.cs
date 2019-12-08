c5de4d3adb448b3799d30a36ade7c9ff5aaefd8e
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2019-12-08 19:07:30 +0100

    RestClient + DB (Read + Write)

---
+++ Soccer Legends/Assets/Scripts/DBManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+
+public class DBManager : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void GetDB()
+    {
+        RestClient.Get<User>("https://soccer-legends-d86c7.firebaseio.com/-LvaWZe8QhmQSlQJ8Njp.json").Then(response =>
+        {
+            text.text = response.userName;
+        });
+    }
+    public void PostDB()
+    {
+        User user = new User("Player1", 10);
+        RestClient.Put("https://soccer-legends-d86c7.firebaseio.com/"+user.userName+".json", user);
+    }
+}

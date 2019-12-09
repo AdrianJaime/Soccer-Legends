85832387c75df175a9de72678bba6b03e45009b6
Adrián Jaime Pastor <36241023+darckball@users.noreply.github.com>
2019-12-09 13:48:29 +0100

    Download from DB

--- Soccer Legends/Assets/Scripts/DBManager.cs
+++ Soccer Legends/Assets/Scripts/DBManager.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Proyecto26;
+using Firebase;
+using Firebase.Storage;
+using System.Threading.Tasks;
+using UnityEngine.Networking;
 
 public class DBManager : MonoBehaviour
 {
     public Text text;
+    Uri url = null;
+    public Image image;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +24,7 @@ public class DBManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(url != null) StartCoroutine(GetImage());
     }
 
     public void GetDB()
@@ -30,5 +38,39 @@ public class DBManager : MonoBehaviour
     {
         User user = new User("Player1", 10);
         RestClient.Put("https://soccer-legends-d86c7.firebaseio.com/"+user.userName+".json", user);
+    }
+    public void DownloadDB()
+    {
+
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        StorageReference gs_reference = storage.GetReferenceFromUrl("gs://soccer-legends-d86c7.appspot.com/Captura.PNG");
+        gs_reference.GetDownloadUrlAsync().ContinueWith((Task<Uri> task) =>
+        {
+            if (!task.IsFaulted && !task.IsCanceled)
+            {
+                Debug.Log("Download URL: " + task.Result);
+                url = task.Result;
+            }
+        });
+    }
+
+    IEnumerator GetImage()
+    {
+        Debug.Log("Hi");
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        url = null;
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            Sprite spr = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), Vector2.zero, 1);
+            image.sprite = spr;
+
+        }
     }
 }

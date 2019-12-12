00233a84f7936ada2ffdf6343a970ebda9c9c80e
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2019-12-12 21:37:35 +0100

    BugFix saves
    Comprobar en este commit la diferencia entre equipo 1 y 2, vaya a ser que guarde todo el rato y no sobreeescriba.

--- Soccer Legends/Assets/Character-Cards/_Scripts/EquipObject.cs
+++ Soccer Legends/Assets/Character-Cards/_Scripts/EquipObject.cs
@@ -1,11 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class EquipObject : MonoBehaviour
 {
     public EquipCardLogic[] arraySlots = new EquipCardLogic[8];
     public EquipData equipData;//array of characters
+    
+    
 
+    private void Start()
+    {
+        EditorUtility.SetDirty(equipData);
+
+    }
 
 }

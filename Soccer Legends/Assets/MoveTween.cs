cd244da7e296d3c3b5bd3960ace3c960d1df1894
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2020-01-27 02:21:01 +0100

    imports y init main menu

---
+++ Soccer Legends/Assets/MoveTween.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTween : MonoBehaviour
+{
+    public float time = 0;
+    public Vector3 movement;
+
+    Transform initialPosition;
+
+    private void Start()
+    {
+        initialPosition = gameObject.transform;
+    }
+    public void Move()
+    {
+        LeanTween.move(gameObject, movement, time);
+    }
+    public void ResetMove()
+    {
+        LeanTween.moveY(gameObject,initialPosition.position.y, time);
+    }
+}

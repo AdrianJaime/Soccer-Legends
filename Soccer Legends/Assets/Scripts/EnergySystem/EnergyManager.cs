adb3a226ca6bd7e06abcc0c44a3b51fae771665e
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2020-02-13 23:20:31 +0100

    MERGE ENERGY BRANCH

---
+++ Soccer Legends/Assets/Scripts/EnergySystem/EnergyManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyManager : MonoBehaviour
+{
+    [SerializeField] EnergyRender energyRender;
+    DateTime nextRestore;
+    int totalEnergy = 0;
+    int maxEnergy = 3;
+    int restoreInterval = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadTimes();
+        StartCoroutine(RestoreFunction());
+    }
+
+    private IEnumerator RestoreFunction()
+    {
+        while (totalEnergy < maxEnergy)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime nextTimeToRestore = nextRestore;
+
+            while (currentTime > nextTimeToRestore)
+            {
+                if (totalEnergy < maxEnergy)
+                {
+                    ///Sumamos energia
+                    totalEnergy++;
+                    ///Añadomos intervalo
+                    nextTimeToRestore = AddTimeInterval(nextTimeToRestore, restoreInterval);
+                    //ACTUALIZAR VARIABLES
+                    nextRestore = nextTimeToRestore;
+                    //Subimos a BD
+                    SaveTimes();
+                }
+                else
+                    break;
+            }
+            if (totalEnergy == maxEnergy)
+                energyRender.UpdateRender(true, totalEnergy);
+            else
+                energyRender.UpdateRender(nextRestore - DateTime.Now,totalEnergy);
+
+            yield return null;
+        }
+    }
+
+    private DateTime AddTimeInterval(DateTime _date, int _timeInterval)
+    {
+        return _date.AddSeconds(_timeInterval);
+    }
+    private void LoadTimes()
+    {
+        //Buscar en BD y asignar variables
+        string dateTimeBD = null;//donde asignaremos la variable
+        if (string.IsNullOrEmpty(dateTimeBD))
+        {
+            nextRestore = DateTime.Now;
+            SaveTimes();
+        }
+        else
+            nextRestore = StringToDate(dateTimeBD);
+    }
+    private void SaveTimes()
+    {
+        //Hacer comprobaciones de tiempos y si todo está correcto 
+        //Guardar en BD los tiempos
+    }
+    private DateTime StringToDate(string date)
+    {
+        if (string.IsNullOrEmpty(date))
+            return DateTime.Now;
+        return DateTime.Parse(date);
+    }
+
+}
---
+++ Soccer Legends/Assets/Scripts/EnergySystem/EnergyRender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyRender : MonoBehaviour
+{
+    [SerializeField] Text energyText, textTimer;
+
+
+    public void UpdateRender(bool _isFull, int _energy)
+    {
+        textTimer.text = "FULL ENERGY";
+        energyText.text = _energy.ToString();
+
+    }
+    public void UpdateRender(TimeSpan timeToWait,int _energy)
+    {
+        string timeToWaitValue = timeToWait.Minutes.ToString() + "min. " + timeToWait.Seconds.ToString() + "s.";
+        textTimer.text = timeToWaitValue;
+        energyText.text = _energy.ToString();
+
+    }
+}

49bca4a8134dee0c0c38dc22c2abdcbbda7041d8
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2020-02-13 23:25:53 +0100

    Merge branch 'Options' into SP-04

---
+++ Soccer Legends/Assets/Scripts/MainMenu/ChangeOption.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeOption : MonoBehaviour
+{
+    [SerializeField] GameObject soundOptions, graphicOptions, extraOptions;
+    [SerializeField] Button soundButton, graphicButton, extraButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundOptions.SetActive(true);
+        graphicOptions.SetActive(false);
+        extraOptions.SetActive(false);
+        soundButton.interactable = false;
+        graphicButton.interactable = true;
+        extraButton.interactable = true;
+
+    }
+
+    public void SetOption(string _name)
+    {
+        switch (_name)
+        {
+            case "sound":
+            case "Sound":
+                soundOptions.SetActive(true);
+                graphicOptions.SetActive(false);
+                extraOptions.SetActive(false);
+
+                soundButton.interactable = false;
+                graphicButton.interactable = true;
+                extraButton.interactable = true;
+                break;
+            case "graphic":
+            case "Graphic":
+                soundOptions.SetActive(false);
+                graphicOptions.SetActive(true);
+                extraOptions.SetActive(false);
+
+                soundButton.interactable = true;
+                graphicButton.interactable = false;
+                extraButton.interactable = true;
+                break;
+            case "extra":
+            case "Extra":
+                soundOptions.SetActive(false);
+                graphicOptions.SetActive(false);
+                extraOptions.SetActive(true);
+
+                soundButton.interactable = true;
+                graphicButton.interactable = true;
+                extraButton.interactable = false;
+                break;
+        }
+    }
+
+}
---
+++ Soccer Legends/Assets/Scripts/UI/ScaleTween.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleTween : MonoBehaviour
+{
+    public float time = 0;
+    public Vector3 finalScale;
+
+    Vector3 initialScale;
+
+    private void Start()
+    {
+        initialScale = gameObject.transform.localScale;
+    }
+    public void Scale()
+    {
+        LeanTween.scale(gameObject, finalScale, time);
+    }
+    public void ResetScale()
+    {
+        LeanTween.scale(gameObject, initialScale, time);
+    }
+}

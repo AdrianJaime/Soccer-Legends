8418ab7164613e78398bbbe48d915fa6149ed27d
Alejandro Luque Espin <alejandro.uzumaki@hotmail.com>
2020-02-27 23:03:53 +0100

    Ui elements charadcter interaction

---
+++ Soccer Legends/Assets/ScrollImage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollImage : MonoBehaviour
+{
+
+    public float scrollVel = 5;
+    public float offset = 100f;
+    Vector2 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+    private void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollVel, offset);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}

8a91930a97363378e13191c73983f3a7e8ea5641
Bartomeu <bartomeu.puig@enti.cat>
2020-06-02 22:20:37 +0200

    corregido un bug del marcador en build

--- Soccer Legends/Assets/Scripts/Gameplay/UI_Gameplay.cs
+++ Soccer Legends/Assets/Scripts/Gameplay/UI_Gameplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using TMPro;
 
 public class UI_Gameplay : MonoBehaviour
 {
@@ -25,23 +26,23 @@ public class UI_Gameplay : MonoBehaviour
 
     public void changeGoalNum(int _ref)
     {
-        TMPro.TextMeshProUGUI score;
+        TextMeshProUGUI score;
         if(_ref == 0)
         {
-            score = transform.GetChild(4).GetComponent<TMPro.TextMeshProUGUI>();
+            score = transform.GetChild(4).GetComponent<TextMeshProUGUI>();
             if (GameObject.Find("Manager").GetComponent<PVE_Manager>() != null)
-                transform.GetChild(3).GetComponent<TMPro.TextMeshProUGUI>().SetText(PlayerPrefs.GetString("username"));
-            else transform.GetChild(3).GetComponent<TMPro.TextMeshProUGUI>().SetText(PhotonNetwork.LocalPlayer.NickName);
+                transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText(PlayerPrefs.GetString("username"));
+            else transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText(PhotonNetwork.LocalPlayer.NickName);
         }
         else
         {
-            score = transform.GetChild(6).GetComponent<TMPro.TextMeshProUGUI>();
+            score = transform.GetChild(6).GetComponent<TextMeshProUGUI>();
             if (GameObject.Find("Manager").GetComponent<PVE_Manager>() != null)
-                transform.GetChild(3).GetComponent<TMPro.TextMeshProUGUI>().SetText(StaticInfo.tournamentTeam.teamName);
-            else transform.GetChild(3).GetComponent<TMPro.TextMeshProUGUI>().SetText(PhotonNetwork.PlayerListOthers[0].NickName);
+                transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText(StaticInfo.tournamentTeam.teamName);
+            else transform.GetChild(3).GetComponent<TextMeshProUGUI>().SetText(PhotonNetwork.PlayerListOthers[0].NickName);
         }
         
-        int scoreNum = int.Parse(score.text.Substring(1));
+        int scoreNum = int.Parse(score.GetParsedText().Substring(1));
         scoreNum++;
         score.SetText("0" + scoreNum.ToString());
 
